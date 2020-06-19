3c69417333cafc861784cd6134aa937280e5cb08
vadim <vadimumnikov@yandex.ru>
2020-06-19 14:10:58 +0300

    identicon docker with python

---
+++ identidock/cmd.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+if [ "$ENV" = 'DEV' ]; then
+	echo "Running Development Server" # start developmend server
+	exec python "identidock.py"
+else
+	echo "Running Production Server" # start prod server set port and remove env in docker-compose for start containers
+	                                 # prod mode
+	exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py \
+	--callable app --stats 0.0.0.0:9191
+fi
